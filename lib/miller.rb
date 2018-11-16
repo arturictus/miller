14ef9cd7bd75970ce1aec1605d4a40a9d325fbef
Artur <arturictus@gmail.com>
2018-11-17 00:37:47 +0700

    DSL working

--- lib/miller.rb
+++ lib/miller.rb
@@ -1,5 +1,94 @@
 require "miller/version"
 
 module Miller
-  # Your code goes here...
+  class NilObject; end
+  def self.with(*attrs)
+
+    opts = extract_options!(attrs)
+
+    config_class = Class.new(Hash) do
+      alias_method :get, :[]
+      alias_method :set, :[]=
+      def self.build(hash)
+        hash.each_with_object(new) do |(k, v), o|
+          o[k] = v
+        end
+      end
+      attrs.each do |attr|
+        define_method attr do
+          get(attr)
+        end
+      end
+    end
+
+    class_methods = Module.new do
+
+      define_method :config do
+        @config ||= config_class.build(opts[:default_config] || {})
+      end
+
+      def _set_config_from_inheritance(config)
+        @config = config
+      end
+
+      attrs.each do |attr|
+        define_method attr do |val = nil, &block|
+          config.set(attr, (block || val))
+        end
+      end
+    end
+
+    instance_module = Module.new do
+      def config
+        self.class.config
+      end
+      attrs.each do |attr|
+        define_method attr do
+          result = instance_variable_get("@#{attr}")
+          if result
+            _extract_config(result)
+          else
+            _extract_config(self.class.config.get(attr))
+          end
+        end
+      end
+      attr_writer *attrs
+
+      private
+
+      def _extract_config(val)
+        if val.respond_to?(:call)
+          instance_exec(&val)
+        else
+          val
+        end
+      end
+    end
+
+    Module.new do
+      singleton_class.send :define_method, :included do |host_class|
+        host_class.extend class_methods
+        host_class.include instance_module
+      end
+      singleton_class.send :define_method, :inherited do |host_class|
+        # return unless self include_module()#
+        # unless dsl.config.empty?
+        binding.pry
+          host_class._set_config_from_inheritance(config)
+        # end
+      end
+    end
+  end
+
+  def self.extract_options!(ary)
+    if ary.last.is_a?(Hash) && extractable_options?(ary.last)
+      ary.pop
+    else
+      {}
+    end
+  end
+
+  def self.extractable_options?(elem)
+    elem.instance_of?(Hash)
+  end
 end
--- spec/miller_spec.rb
+++ spec/miller_spec.rb
@@ -3,7 +3,87 @@ RSpec.describe Miller do
     expect(Miller::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'adding DSL to a class' do
+    it 'example' do
+      klass = Class.new do
+        include Miller.with(:name, :lastname)
+        name 'John'
+        lastname 'Doe'
+      end
+      expect(klass.config.name).to eq 'John'
+      expect(klass.config.lastname).to eq 'Doe'
+      inst = klass.new
+      expect(inst.name).to eq 'John'
+      expect(inst.lastname).to eq 'Doe'
+    end
+    it 'blocks' do
+      klass = Class.new do
+        include Miller.with(:name, :lastname)
+        name { another_name }
+        lastname 'Doe'
+
+        def another_name
+          "victor"
+        end
+      end
+      expect(klass.config.name).to be_a Proc
+      expect(klass.config.lastname).to eq 'Doe'
+      inst = klass.new
+      expect(inst.name).to eq 'victor'
+      expect(inst.lastname).to eq 'Doe'
+    end
+    it 'default config' do
+      klass = Class.new do
+        include Miller.with(:name, :lastname, default_config: { name: 'Henry', lastname: 'Miller' })
+        lastname 'Doe'
+      end
+      expect(klass.config.name).to eq 'Henry'
+      expect(klass.config.lastname).to eq 'Doe'
+      inst = klass.new
+      expect(inst.name).to eq 'Henry'
+      expect(inst.lastname).to eq 'Doe'
+    end
+    it 'instance override' do
+      klass = Class.new do
+        include Miller.with(:name, :lastname)
+        name 'John'
+        lastname 'Doe'
+      end
+      expect(klass.config.name).to eq 'John'
+      expect(klass.config.lastname).to eq 'Doe'
+      inst = klass.new
+      inst.name = 'Henry'
+      expect(inst.name).to eq 'Henry'
+      expect(inst.lastname).to eq 'Doe'
+    end
+    it 'instance override with block' do
+      klass = Class.new do
+        include Miller.with(:name, :lastname)
+        name 'John'
+        lastname 'Doe'
+        def another_name
+          'Martha'
+        end
+      end
+      expect(klass.config.name).to eq 'John'
+      expect(klass.config.lastname).to eq 'Doe'
+      inst = klass.new
+      inst.name = proc { another_name }
+      expect(inst.name).to eq 'Martha'
+      expect(inst.lastname).to eq 'Doe'
+    end
+    it 'inheritance' do
+      super_klass = Class.new do
+        include Miller.with(:name, :lastname)
+        name 'John'
+        lastname 'Doe'
+      end
+      klass = Class.new(super_klass)
+      expect(klass.config.name).to eq 'John'
+      expect(klass.config.lastname).to eq 'Doe'
+      inst = klass.new
+      expect(inst.name).to eq 'John'
+      expect(inst.lastname).to eq 'Doe'
+    end
   end
 end
