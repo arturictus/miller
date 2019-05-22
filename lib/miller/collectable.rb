6ca658271a5d0f9474834287ea1e617acfe0b6aa
Artur Panach <arturictus@gmail.com>
2019-05-22 22:24:05 +0200

    improving collectable

--- lib/miller/collectable.rb
+++ lib/miller/collectable.rb
@@ -1,27 +1,42 @@
 module Miller
   class Collectable
-    def self.data
-      @data ||= { }
+    def self._collectables
+      @_collectables ||= { }
     end
 
-    def data
-      self.class.data
+    def _collectables
+      self.class._collectables
     end
 
     def self.collectable(name, klass)
       singleton_class.define_method name do |&block|
-        self.data ||= {}
-        self.data[name] ||= [] 
-        self.data[name] << Class.new(klass, &block)
+        _set_collectable(name, klass, [])
+        self._collectables[name] << Class.new(klass, &block)
       end
+      _gen_accessor(name)
     end
 
     def self.named_collectable(col_name, klass)
       singleton_class.define_method col_name do |name, &block|
-        self.data ||= {}
-        self.data[col_name] ||= {}
-        self.data[col_name][name] = Class.new(klass, &block)
+        _set_collectable(col_name, klass, {})
+        self._collectables[col_name][name] = Class.new(klass, &block)
+      end
+      _gen_accessor(col_name)
+    end
+
+    def self._set_collectable(name, klass, default)
+      self._collectables ||= {}
+      self._collectables[name] ||= default 
+    end
+
+    def self._gen_accessor(name)
+      singleton_class.define_method "#{name}_col" do
+        self._collectables[name]
+      end
+      define_method "#{name}_col" do
+        self.class._collectables[name]
       end
     end
+
   end
 end 
--- spec/collectable_spec.rb
+++ spec/collectable_spec.rb
@@ -4,10 +4,14 @@ module Miller
     class Attribute < Miller.base(:name, :lastname)
     end
     class ColBaseExample < Collectable
+      include Miller.with(:name, :version)
       collectable :property, Attribute
       named_collectable :deployment, Attribute
     end
     class ColExample < ColBaseExample
+      name :my_name
+      version "0.1.0"
+      
       property do
         name "hello"
       end
@@ -18,15 +22,24 @@ module Miller
         name :app
       end
     end
-    
+
     it "generates proper metadata" do
       inst = ColExample.new
-      expect(ColExample.data[:property].count).to eq 2
-      expect(inst.data[:property].count).to eq 2
-      expect(inst.data[:property].first.ancestors).to include(Miller::Attribute)
-      expect(ColExample.data[:deployment][:app]).not_to be_nil
-      expect(inst.data[:deployment][:app]).not_to be_nil
-      expect(inst.data[:deployment][:app].ancestors).to include(Miller::Attribute)
+      expect(ColExample._collectables[:property].count).to eq 2
+      expect(inst._collectables[:property].count).to eq 2
+      expect(inst._collectables[:property].first.ancestors).to include(Miller::Attribute)
+      expect(ColExample._collectables[:deployment][:app]).not_to be_nil
+      expect(inst._collectables[:deployment][:app]).not_to be_nil
+      expect(inst._collectables[:deployment][:app].ancestors).to include(Miller::Attribute)
+    end
+
+    it "own properties" do
+      expect(ColExample.config.name).to eq(:my_name)
+      expect(ColExample.new.name).to eq(:my_name)
+    end
+    it "accessors" do
+      expect(ColExample.property_col.count).to eq 2
+      expect(ColExample.new.property_col.count).to eq 2
     end
   end
 end
